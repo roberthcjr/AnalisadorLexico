5e6c6267136bea2c715efab64275a0e29ee2d1a5
Robert <roberthcjr1999@gmail.com>
2023-10-27 20:14:06 -0300

    Added a full test

--- src/main/java/src/TesteALex.java
+++ src/main/java/src/TesteALex.java
@@ -8,9 +8,20 @@ public class TesteALex {
 
     public static void main(String[] args) throws IOException{
 
-        File arquivoTeste = new File("./testes/teste1.txt");
+//        File arquivoTeste = new File("./testes/teste1.txt");
 
-        String teste = "program teste1\n";
+        String teste = "program teste1\n" +
+                "declare\n" +
+                "    integer a, b, c;\n" +
+                "    integer result;\n" +
+                "\"Aqui tem um literal\"" +
+                "begin\n" +
+                "    read (a);\n" +
+                "    read (c);\n" +
+                "    b := 10;\n" +
+                "    result := (a * c)/(b + 5 % 345 -3) ;\n" +
+                "    write(result)\n" +
+                "end";
 
         AnalisadorLexico lexical = new AnalisadorLexico(new StringReader(teste));
         lexical.yylex();
